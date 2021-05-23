867ea7a276ea0484d41e6ac80fe067a759432c19
Ian <iankwanhou@gmail.com>
2021-05-23 00:34:07 -0400

    welcome

--- app.py
+++ app.py
@@ -18,6 +18,11 @@ import math
 app = Flask(__name__)
 
 
+@app.route('/')
+def welcome():
+    'Welcome to DHElios'
+
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
